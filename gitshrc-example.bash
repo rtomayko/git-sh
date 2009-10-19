982b147174c192060f97b642830bed5d728755ac
Ryan Tomayko <r@tomayko.com>
2009-10-19 15:01:06 -0700

    update example ~/.gitshrc with latest from my home dir

--- gitshrc-example.bash
+++ gitshrc-example.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Example ~/.gitshrc file.
+# rtomayko's ~/.gitshrc file
 
 # git commit
 gitalias commit='git commit --verbose'
@@ -7,6 +7,10 @@ gitalias amend='git commit --verbose --amend'
 gitalias ci='git commit --verbose'
 gitalias ca='git commit --verbose --all'
 gitalias  n='git commit --verbose --amend'
+
+# git branch and remote
+gitalias  b='git branch'
+gitalias rv='git remote -v'
 
 # git add
 gitalias  a='git add'
@@ -19,7 +23,7 @@ gitalias c='git checkout'
 # git fetch
 gitalias f='git fetch'
 
-# git rebase
+# basic interactive rebase of last 10 commits
 gitalias r='git rebase --interactive HEAD~10'
 
 # git diff
@@ -33,6 +37,9 @@ alias  s='git status'
 gitalias  L='git log'
 gitalias  l='git log --pretty=oneline --abbrev-commit'
 gitalias ll='git log --pretty=oneline --abbrev-commit --max-count=15'
+
+# misc
+gitalias pick='git cherry-pick'
 
 # experimental
 gitalias mirror='git reset --hard'
