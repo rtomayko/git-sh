8051b38974c12a317e24be641aff5ec653df56be
Nik Reiman <nik.reiman@gmail.com>
2023-05-31 16:13:31 +0000

    Remove (unused) prompt stuff
    This should now be set by ~/.gitshrc

--- git-sh.bash
+++ git-sh.bash
@@ -175,20 +175,6 @@ _git_import_aliases () {
 	)"
 }
 
-# PROMPT =======================================================================
-
-case $(uname) in
-  MINGW*)
-    # Some git commands are REALLY SLOW on Windows, so use a stripped-down PS1
-    # PS1='`_git_headname`!`_git_repo_state``_git_workdir``_git_dirty`> '
-    # Even more stripped...
-    # PS1='`_git_headname`!`_git_workdir`> '
-    ;;
-  *)
-    PS1='`_git_headname``_git_upstream_state`!`_git_repo_state``_git_workdir``_git_dirty``_git_dirty_stash`> '
-    ;;
-esac
-
 ANSI_RESET="\001$(git config --get-color "" "reset")\002"
 
 # detect whether the tree is in a dirty state.
